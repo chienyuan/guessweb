af31a218e2263b9701243022491be313da34b982
Eric Feng <chienyuan.feng@gmail.com>
2014-02-12 13:44:12 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guessweb::Application.config.secret_key_base = '6ff78ea228e89dd57036fa5979b8e709ee9d78bd18271f0eff4969b9bd8afd2b5467f7590731c246a1d315cdaa838170159cde2313e5c5d9f719d4d181ad3fbf'
